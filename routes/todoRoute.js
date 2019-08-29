bc4f704f7c06726175f1b80f5c9ab601cb26b95b
HaroldSenier <harold.senier@transcom.com>
2019-08-29 18:00:06 +0800

    remove validator in updateTodo

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -5,7 +5,7 @@ const validator = require("../validators/todoValidator");
 
 router.get("/todo", getTodos);
 router.post("/todo/create", validator.createTodoValidator, createTodo);
-router.post("/todo/update", validator.createTodoValidator, updateTodo);
+router.post("/todo/update", updateTodo);
 
 
 module.exports = router;
