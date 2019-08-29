6758316329ea58fae98a7251c969502291f66b6a
HaroldSenier <harold.senier@transcom.com>
2019-08-29 17:33:03 +0800

    Add Update Todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,7 @@
 const Todo = require("../models/todoModel");
 
 exports.getTodos = (req, res) => {
-
+  console.log("RETRIEVING TODO", req.body);
   const queryFilters = req.query;
   const todo = Todo.find({...queryFilters})
   .then(todos => {
@@ -13,12 +13,21 @@ exports.getTodos = (req, res) => {
     console.log("err");
     return res.err;
   });
-
 };
 
 exports.createTodo = (req, res) => {
   const todo = new Todo(req.body);
   console.log("CREATING TODO", req.body);
+  todo.save().then(result => {
+    return res.json({
+      todo: result
+    });
+  });
+};
+
+exports.updateTodo = (req, res) => {
+  const todo = req.body;
+  console.log("UPDATING TODO", req.body);
   todo.save().then(result => {
     return res.json({
       todo: result
